c4403ca0fdc751c5723123627cb5cdb24043fbab
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-04-22 11:50:43 +0200

    added movieController

---
+++ controllers/movieController.js
@@ -0,0 +1,31 @@
+const connection = require("../data/linkup")
+
+const index = (req, res) => {
+    const mysql = 'SELECT * FROM movies'
+    connection.query(mysql, (err, results) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({ error: "Database Error" })
+        }
+        res.json(results)
+    })
+}
+
+const show = (req, res) => {
+
+    const mysql = 'SELECT * FROM movies WHERE id = ?'
+    const { id } = req.params
+
+    connection.query(mysql, [id], (err, results) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({ error: 'Database error' })
+        }
+        res.json(results[0])
+    })
+}
+
+module.exports = {
+    index,
+    show
+}
--- routes/movies.js
+++ routes/movies.js
@@ -1,33 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const connection = require("../data/linkup")
+const moviecontroller = require("../controllers/movieController")
 
 
-router.get('/', (req, res) => {
-    const mysql = 'SELECT * FROM movies';
-    connection.query(mysql, (err, results) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: "Database error" });
-        }
-        res.json(results);
-    });
-});
+router.get('/', moviecontroller.index)
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    const mysql = 'SELECT * FROM movies WHERE id = ?';
-
-    connection.query(mysql, [id], (err, results) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: "Database error" });
-        }
-        if (results.lenght === 0) {
-            return res - status(404).json({ error: "Movie not found" });
-        }
-        res.json(results[0]);
-    });
-});
+router.get('/:id', moviecontroller.show)
 
 module.exports = router;
