2ff1dc0fb1924f8d8ee0b41cf40e9c4d1dbd0ebe
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-04-22 12:07:34 +0200

    added public folder for images, added reviews to show

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const movieRoute = require("./routes/movies")
 require('dotenv').config()
+app.use(express.static('public'))
 
 const PORT = process.env.PORT || "my_port"
 
