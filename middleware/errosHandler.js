33514aac2adb42464bd9d5747031d66296fdb048
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-04-22 12:14:35 +0200

    added middleware

---
+++ middleware/errosHandler.js
@@ -0,0 +1,6 @@
+const errorsHandler = (err, req, res, next) => {
+    console.error(err)
+    res.status(500).json({ error: "Internal Server Error" })
+}
+
+module.exports = errorsHandler
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(express.static('public'))
 const PORT = process.env.PORT || "my_port"
 
 const connection = require("./data/linkup")
+const errorsHandler = require('./middleware/errosHandler')
+const notFound = require('./middleware/notFound')
 
 app.listen(PORT, () => {
     console.log(`Post API listening on http://127.0.0.1:${PORT}`);
@@ -17,3 +19,6 @@ app.get('/', (req, res) => {
 })
 
 app.use('/movies', movieRoute)
+
+app.use(notFound)
+app.use(errorsHandler)
